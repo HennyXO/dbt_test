3e451965f0b16981996ed595a29096bb63f5ced8
leohennessy16@gmail.com <leohennessy16@gmail.com>
2026-03-17 00:05:20 +0000

    added data tests

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,22 @@
+with 
 
-select
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
-from {{source('jaffle_shop','orders')}}
+        status as order_status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
