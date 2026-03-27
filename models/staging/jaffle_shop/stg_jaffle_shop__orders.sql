fbb10e1d66eb62526c85d2ae79a17acddd1614ff
Leo Hennessy <leohennessy16@gmail.com>
2026-03-27 05:51:08 +0000

    finishing refactoring sql course

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -13,7 +13,12 @@ renamed as (
         user_id as customer_id,
         order_date,
         status as order_status,
-        _etl_loaded_at
+        case
+            when status not in ('returned','return_pending')
+            then order_date
+            end as valid_order_date,
+        _etl_loaded_at,
+        row_number() over (partition by user_id order by order_date, id) as user_order_seq
 
     from source
 
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -11,7 +11,7 @@ SELECT
   orderid as order_id,
   paymentmethod as payment_method,
   status as payment_status,
-  amount as payment_amount,
+  round(amount/100.0,2) as payment_amount,
   created as payment_created,
   _batched_at
 FROM source
